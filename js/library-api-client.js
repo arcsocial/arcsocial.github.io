9dcb42d60a50c0a8e89f4280894aed98edb2af34
arcsocial <arcsoocial@gmail.com>
2025-06-10 09:44:23 +0530

    Update library-api-client.js
    Marker for numeric book numbers

--- js/library-api-client.js
+++ js/library-api-client.js
@@ -415,8 +415,12 @@ function displayBooks(books) {
     }
     
     html += `<div id="${bookid}" class="book-item">
-              <strong>${book.Title}</strong> | ${book.Author} | <strong>${book.Genre}</strong>
-            </div>`;
+              <strong>${book.Title}</strong> | ${book.Author} | <strong>${book.Genre}</strong>';
+    if ( !Number.isNaN(book.Number) ) {
+      html+= '*</div>`;
+    } else {
+      html+= '</div>`;
+    }
     count++;      
   });
 
